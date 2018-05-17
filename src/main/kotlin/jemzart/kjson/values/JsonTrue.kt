38a1d8bb613a328eaf9558f00a8bd32fd149f86c
Facundo <zogar1993@hotmail.com>
2018-05-17 20:40:24 -0300

    Parsing loggic extracted from main file. Main file left for API exposal.

--- src/main/kotlin/jemzart/kjson/values/JsonTrue.kt
+++ src/main/kotlin/jemzart/kjson/values/JsonTrue.kt
@@ -1,6 +1,6 @@
 package jemzart.kjson.values
 
-class JsonTrue  internal constructor() : JsonLiteral() {
+class JsonTrue internal constructor() : JsonLiteral() {
 	override val value = true
 	override fun toString() = "true"
 }
