3a0b3677d5f29232fea5a79d78135484120127de
Facundo <zogar1993@hotmail.com>
2018-05-18 17:16:43 -0300

    Se elimina codigo innecesario.

--- src/main/kotlin/jemzart/kjson/values/JsonArray.kt
+++ src/main/kotlin/jemzart/kjson/values/JsonArray.kt
@@ -5,11 +5,6 @@ import jemzart.kjson.JSON_VALUE
 class JsonArray internal constructor() : JsonValue() {
 	private val list: MutableList<Any?> = mutableListOf()
 
-	override operator fun get(key: String) = throw UnsupportedOperationException()
-	override fun set(key: String, value: JsonValue) = throw UnsupportedOperationException()
-	override fun <T> get(key: String, shamelessHack: T): T = throw UnsupportedOperationException()
-	override fun set(key: String, value: Any?) = throw UnsupportedOperationException()
-
 	override fun iterator(): Iterator<Any?> = list.iterator()
 
 	override fun <T> get(index: Int, shamelessHack: T): T = list[index] as T
@@ -18,7 +13,6 @@ class JsonArray internal constructor() : JsonValue() {
 	override fun set(index: Int, value: Any?) {
 		list[index] = value
 	}
-	override fun set(index: Int, value: JsonValue) = set(index, value as Any)
 
 	override fun toString(): String {
 		val stringBuilder = StringBuilder()
--- src/main/kotlin/jemzart/kjson/values/JsonObject.kt
+++ src/main/kotlin/jemzart/kjson/values/JsonObject.kt
@@ -7,17 +7,11 @@ class JsonObject internal constructor() : JsonValue() {
 	private val map: MutableMap<String, Any?> = mutableMapOf()
 	private val list: LinkedList<Pair<String, Any?>> = LinkedList()
 
-	override fun get(index: Int): JsonValue = throw UnsupportedOperationException()
-	override fun set(index: Int, value: JsonValue) = throw UnsupportedOperationException()
-	override fun <T> get(index: Int, shamelessHack: T): T = throw UnsupportedOperationException()
-	override fun set(index: Int, value: Any?) = throw UnsupportedOperationException()
-
 	override fun iterator(): Iterator<Any?> = list.map { it.second } .iterator()
 
 	override fun <T> get(key: String, shamelessHack: T): T = map[key] as T
 	override fun get(key: String): JsonValue = get(key, JSON_VALUE)
 
-	override fun set(key: String, value: JsonValue) = set(key, value as Any)
 	override fun set(key: String, value: Any?){
 		if(map.containsKey(key)) {
 			val obj = map[key]
