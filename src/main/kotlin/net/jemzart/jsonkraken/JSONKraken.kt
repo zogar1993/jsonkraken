c5f62b0924923636526b25a89b670f8c2b87aa4d
Facundo <zogar1993@hotmail.com>
2018-11-03 05:45:47 -0300

    Added Dokka javadocs to all extension methods exposed. Added Dokka javadocs to all circular reference exception.

--- src/main/kotlin/net/jemzart/jsonkraken/JSONKraken.kt
+++ src/main/kotlin/net/jemzart/jsonkraken/JSONKraken.kt
@@ -6,33 +6,71 @@ import net.jemzart.jsonkraken.values.JsonArray
 import net.jemzart.jsonkraken.values.JsonObject
 import net.jemzart.jsonkraken.values.JsonValue
 
+/**
+ * @return easy way to convert an Iterable to a JsonArray.
+ * @receiver an iterable containing only valid types (JsonValue, null and all primitives are valid types).
+ */
 fun Iterable<*>.toJsonArray(): JsonArray {
 	val jsonArray = JsonArray()
 	for (item in this) jsonArray.add(item)
 	return jsonArray
 }
 
+/**
+ * @return easy way to convert a map to a JsonObject.
+ * @receiver a map containing only valid types (JsonValue, null and all primitives are valid types).
+ */
 fun Map<String, *>.toJsonObject(): JsonObject {
 	val jsonObject = JsonObject()
 	for (pair in this) jsonObject[pair.key] = pair.value
 	return jsonObject
 }
 
+/**
+ * @return an object representation of the receiver.
+ * @receiver raw json data.
+ */
 fun String.toJson(): Any? = StringToObjectParser(this).create()
+
+/**
+ * @param formatted if false, needless blank spaces will be removed, if true, the result will be more human readable.
+ * @return an json string representation of the receiver.
+ * @receiver an object of any valid type (JsonValue, null and all primitives are valid types).
+ */
 fun Any?.toJsonString(formatted: Boolean = false): String = ObjectToStringParser(this, formatted).create()
 
+/**
+ * @return Gets the value of the property named [name] in a JsonValue.
+ * @receiver if JsonArray, [name] works as an index.
+ * @exception <UnsupportedOperationException> if receiver is not a JsonValue.
+ */
 operator fun Any?.get(name: String): Any? =
 	if (this is JsonValue) this[name]
 	else throw UnsupportedOperationException()
 
+/**
+ * @return Gets the element at index [index] in a JsonValue.
+ * @receiver if JsonObject, [index] works as a property name.
+ * @exception <UnsupportedOperationException> if receiver is not a JsonValue.
+ */
 operator fun Any?.get(index: Int): Any? =
 	if (this is JsonValue) this[index]
 	else throw UnsupportedOperationException()
 
+/**
+ * Sets [value] in a property named [name] in a JsonValue.
+ * @receiver if JsonArray, [name] works as an index.
+ * @exception <UnsupportedOperationException> if receiver is not a JsonValue.
+ */
 operator fun Any?.set(name: String, value: Any?) =
 	if (this is JsonValue) this[name] = value
 	else throw UnsupportedOperationException()
 
+/**
+ * Sets [value] in the selected [index] in a JsonValue.
+ * @receiver if JsonObject, [index] works as a property name.
+ * @exception <UnsupportedOperationException> if receiver is not a JsonValue.
+ */
 operator fun Any?.set(index: Int, value: Any?) =
 	if (this is JsonValue) this[index] = value
 	else throw UnsupportedOperationException()
--- src/main/kotlin/net/jemzart/jsonkraken/exceptions/CircularReferenceException.kt
+++ src/main/kotlin/net/jemzart/jsonkraken/exceptions/CircularReferenceException.kt
@@ -1,7 +1,16 @@
 package net.jemzart.jsonkraken.exceptions
 
 import net.jemzart.jsonkraken.values.JsonValue
-
+/**
+ * An exception used for operations that would incur in a circular reference.
+ *
+ * Logically, this can only occur when assigning a JsonValue to another.
+ *
+ * @param T the type of a member in this group.
+ * @property host the name of this group.
+ * @property intruder the name of this group.
+ * @constructor Creates an empty group.
+ */
 class CircularReferenceException(val host: JsonValue,
                                  val intruder: JsonValue)
 	: Exception("Performing the operation would incur in a circular reference.")
