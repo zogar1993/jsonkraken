579a90d6f356a23cee7b7b6fb8ad1dcdab29dcb4
Facundo <zogar1993@hotmail.com>
2019-02-22 21:09:54 -0300

    Added internal to the inline string, since it is for deserialization only.

--- src/main/kotlin/net/jemzart/jsonkraken/wrappers/BoundedString.kt
+++ src/main/kotlin/net/jemzart/jsonkraken/wrappers/BoundedString.kt
@@ -2,7 +2,7 @@ package net.jemzart.jsonkraken.wrappers
 
 import net.jemzart.jsonkraken.exceptions.TokenExpectationException
 
-inline class BoundedString(val value: String){
+internal inline class BoundedString(val value: String){
 	operator fun get(index: Int): Char =
 		if (index < value.length) value[index]
 		else throw TokenExpectationException("Premature end of String")
