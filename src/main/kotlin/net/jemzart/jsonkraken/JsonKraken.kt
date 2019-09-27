f609c02cc882ca2dc49e46203b5ac1a94e93514e
Ragoz <zogar1993@hotmail.com>
2019-09-27 20:16:32 -0300

    Added nice kdocs for JsonKraken API

--- src/main/kotlin/net/jemzart/jsonkraken/JsonKraken.kt
+++ src/main/kotlin/net/jemzart/jsonkraken/JsonKraken.kt
@@ -8,20 +8,33 @@ import net.jemzart.jsonkraken.values.JsonValue
 
 object JsonKraken {
 	/**
-	 * @return a JsonValue representation of [data].
+	 * @param [data] is a raw String representation of a JSON.
+	 * @param [T] is the expected representation of [data].
+	 * @return a [T] representation of [data].
+	 * @since 2.0
 	 */
 	inline fun <reified T: JsonValue>deserialize(data: String): T {
 		val raw = Deserializer(data).create()
 		return cast(raw)
 	}
 
+	/**
+	 * @param [value] will be converted to its String JSON representation.
+	 * @param [formatted] will format the resulting JSON representation if true.
+	 * @return a String with the serialized JSON representation of [value].
+	 * @throws [InvalidCastException] See 'Valid Types' for more information.//TODO mejorar con link
+	 * @since 2.0
+	 */
 	fun serialize(value: Any?, formatted: Boolean = false): String {
 		return Serializer(value.purify(), formatted).create()
 	}
 
 	/**
-	 * @return a JsonValue representation of [value].
-	 * @receiver the object to be converted.
+	 * @param [value] will be converted to [T] representation.
+	 * @param [T] is the expected representation of [value] .
+	 * @return a [T] representation of [value].
+	 * @throws [InvalidCastException] See 'Valid Types' for more information.//TODO mejorar con link
+	 * @since 2.0
 	 */
 	inline fun <reified T: JsonValue> transform(value: Any?): T {
 		val result = value.purify()
