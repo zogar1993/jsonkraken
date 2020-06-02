cb52ce37e2d21c3c02350b9fff56df85d9583b41
Ragoz <zogar1993@hotmail.com>
2020-06-02 00:49:02 -0300

    Refactor.

--- src/main/kotlin/net/jemzart/jsonkraken/deserializer/throws/throwExpectationFailed.kt
+++ src/main/kotlin/net/jemzart/jsonkraken/deserializer/throws/throwExpectationFailed.kt
@@ -2,7 +2,7 @@ package net.jemzart.jsonkraken.deserializer.throws
 
 import net.jemzart.jsonkraken.deserializer.Deserializer
 
-internal fun Deserializer.throwExpectationFailed(char: Char, expected: Array<Char>) {
+internal fun Deserializer.throwExpectationFailed(char: Char, expected: Array<Char>): Nothing {
 	val expectation = "[${expected.joinToString(",")}]"
 	throwError("Expected one of ${expectation}, found \"$char\".")
 }
